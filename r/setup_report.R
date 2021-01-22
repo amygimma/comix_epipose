95dd42aed4ad4f13fdff396b6f0f3a47271a69bc
amyg225 <amyg225@gmail.com>
2021-01-22 15:18:09 +0000

    contact means by age group and more setup

--- r/setup_report.R
+++ r/setup_report.R
@@ -25,14 +25,18 @@ ggthemr::ggthemr("grape")
 # Read participants data ---------
 
 part <- qread(file.path(here::here(), path_to_folder, "participants.qs"))
-part <- part %>% filter(country == country_code_)
-# table(part$country)
+table(part$part_age_group)
+part <- part %>% filter(country == country_code_) %>%
+  mutate(part_age_group = ifelse(part_age_group == "70-120", "70+", part_age_group))
+table(part$part_age_group)
+
+table(part$country, part$wave)
 
 # Read contacts data -----------
 
 contacts <- qread(file.path(here::here(), path_to_folder, "contacts.qs"))
 contacts <- contacts %>% filter(country == country_code_)
-# table(contacts$country)
+table(contacts$country, contacts$wave)
 
 
 # Read households data ----------
